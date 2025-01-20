1f2343eb45f27f483c38d301e0233f272ecb2d73
DivineDemon <supame123@gmail.com>
2025-01-20 19:18:15 +0500

    Fixes!

--- src/env.ts
+++ src/env.ts
@@ -34,7 +34,7 @@ export const env = createEnv({
     STRIPE_WEBHOOK_SECRET: z
       .string()
       .regex(
-        /^whsec_[a-zA-Z0-9]{64}$/,
+        /^whsec_[a-zA-Z0-9]{32}$/,
         "Invalid Stripe Webhook Secret Format!"
       ),
   },
