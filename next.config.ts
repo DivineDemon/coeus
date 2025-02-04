3cfccdb1e6244209c8e61e33aab71027c95d86fc
Mushood Mohammed Hanif <supame123@gmail.com>
2025-02-05 02:48:48 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,31 +2,31 @@ import "./src/env.ts";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "gravatar.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ui.shadcn.com",
+      },
+    ],
+  },
   experimental: {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "avatars.githubusercontent.com",
-        },
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "lh3.googleusercontent.com",
-        },
-        {
-          protocol: "https",
-          hostname: "gravatar.com",
-        },
-        {
-          protocol: "https",
-          hostname: "ui.shadcn.com",
-        },
-      ],
-    },
     serverActions: {
       bodySizeLimit: "3mb",
     },
